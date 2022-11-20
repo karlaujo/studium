0bffba531deef2cebce51cf71f3168a80b800342
theSaab <saba.fathi@emailgmail.info>
2022-11-20 08:37:20 -0500

    api keys

---
+++ src/config.js
@@ -0,0 +1,6 @@
+const config = {
+    REACT_APP_GOOGLE_API_KEY: "AIzaSyCNeyBUPE6ktrmN9doUtzDwwySuQJXG-yc",
+    REACT_APP_RAPID_API_KEY: "be7c418c50msha1e43daba45b312p16a697jsn6771e5be6f14",
+}
+
+export default config;
